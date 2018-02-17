d278c41d0b1e6109e3f08bb0a13aa6ac9f96dc3a
Miguel Sanchez <miguelsanchezangel@yahoo.com>
2018-02-17 15:51:46 -0800

    first commit

--- VoiceRehabilitation/app/src/main/java/team4/com/voicerehabilitation/MainActivity.java
+++ VoiceRehabilitation/app/src/main/java/team4/com/voicerehabilitation/MainActivity.java
@@ -1,13 +1,36 @@
 package team4.com.voicerehabilitation;
 
+import android.graphics.PixelFormat;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    VideoView playVideo;
+    Uri uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        playVideo = findViewById(R.id.videoView1);
+        uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.a);
+        playVideo.setVideoURI(uri);
+
+        Button playButton = findViewById(R.id.button1);
+        playButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(View v){
+                playVideo.start();
+            }
+        });
+
+
     }
 }
