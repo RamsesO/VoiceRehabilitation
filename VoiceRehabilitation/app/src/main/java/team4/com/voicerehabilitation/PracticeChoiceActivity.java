2b57838ad5c0646b17e3015679161c1c0542924c
Armine Khachatryan <armine.khachatryan94@yahoo.com>
2018-02-17 17:55:12 -0800

    vowels on practice page to sounds page

--- VoiceRehabilitation/app/src/main/java/team4/com/voicerehabilitation/PracticeChoiceActivity.java
+++ VoiceRehabilitation/app/src/main/java/team4/com/voicerehabilitation/PracticeChoiceActivity.java
@@ -1,7 +1,10 @@
 package team4.com.voicerehabilitation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PracticeChoiceActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class PracticeChoiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice_choice);
+
+        Button btn = (Button)findViewById(R.id.vowelsButton);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PracticeChoiceActivity.this, SoundChoiceActivity.class));
+            }
+        });
+
+
     }
+
+
 }
