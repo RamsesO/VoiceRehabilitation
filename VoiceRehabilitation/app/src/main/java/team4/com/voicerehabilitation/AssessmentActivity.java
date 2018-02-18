39a53e67235a82b6b729d0e0c8ced5edad1a5424
Miguel Sanchez <miguelsanchezangel@yahoo.com>
2018-02-18 09:25:31 -0800

    merged

--- VoiceRehabilitation/app/src/main/java/team4/com/voicerehabilitation/AssessmentActivity.java
+++ VoiceRehabilitation/app/src/main/java/team4/com/voicerehabilitation/AssessmentActivity.java
@@ -83,13 +83,54 @@ public class AssessmentActivity extends AppCompatActivity implements OnChartValu
         {
             case R.id.eeBtn:
                 uriParse = "android.resource://" + getPackageName() + "/" + R.raw.ae;
-                setTitle("Assessing a");
-
+                setTitle("Assessing: /i/ - (ee)");
                 break;
+
             case R.id.iBtn:
                 uriParse = "android.resource://" + getPackageName() + "/" + R.raw.a;
-                setTitle("Assessing i");
+                setTitle("Assessing: /l/-(i)");
                 break;
+
+            case R.id.eBtn:
+                uriParse = "android.resource://" + getPackageName() + "/" + R.raw.a;
+                setTitle("Assessing: / /-(e)");
+                break;
+
+            case R.id.aeBtn:
+                uriParse = "android.resource://" + getPackageName() + "/" + R.raw.ae;
+                setTitle("Assessing: /ae/-(ae)");
+                break;
+
+            case R.id.ahBtn:
+                uriParse = "android.resource://" + getPackageName() + "/" + R.raw.a;
+                setTitle("Assessing: /a/-(ah)");
+                break;
+
+            case R.id.awBtn:
+                uriParse = "android.resource://" + getPackageName() + "/" + R.raw.ae;
+                setTitle("Assessing: /ə/-(aw)");
+                break;
+
+            case R.id.ûBtn:
+                uriParse = "android.resource://" + getPackageName() + "/" + R.raw.a;
+                setTitle("Assessing: /ʊ/-(û)");
+                break;
+
+            case R.id.ooBtn:
+                uriParse = "android.resource://" + getPackageName() + "/" + R.raw.ae;
+                setTitle("Assessing: /u/-(oo)");
+                break;
+
+            case R.id.uBtn:
+                uriParse = "android.resource://" + getPackageName() + "/" + R.raw.a;
+                setTitle("Assessing: /ʌ/-(u)");
+                break;
+
+            case R.id.erBtn:
+                uriParse = "android.resource://" + getPackageName() + "/" + R.raw.a;
+                setTitle("Assessing: /ɛ/-(er)");
+                break;
+
             default:
                 System.out.println("went to default");
                 break;
